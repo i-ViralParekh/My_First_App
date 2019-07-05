26922611c47c70930b604df2c129c64ba8bc1ffd
i-ViralParekh <viral.parekh@mirraw.com>
2019-07-05 14:50:07 +0530

    Activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,25 +14,17 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
 
 
 
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :test
+  host = 'hidden-falls-83213.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+ 
 
 
 
